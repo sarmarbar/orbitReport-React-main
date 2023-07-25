efd1a21ae9a6f728c187e55dc55b7d707dfe1661
sarmarbar <sarah.smb.barker@gmail.com>
2023-07-24 22:43:29 -0500

    My first commit

---
+++ OneDrive/Documents/Projects/LaunchCode/orbitReport-React-main/src/components/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+//import Banner from "./components/Banner";
+
+export default function Banner() {
+    return (
+        <header>
+            <h1 id='banner'>Orbit Report</h1>
+            <p>Click on the buttons to see the satellites in that orbit type</p>
+        </header>
+    )
+}
---
+++ OneDrive/Documents/Projects/LaunchCode/orbitReport-React-main/src/components/Buttons.js
@@ -0,0 +1,16 @@
+import satData from "./satData.js";
+
+//import React from 'react'
+//import App from './Tests/App';
+
+const Buttons = () => {
+  
+   return (
+   <div>
+      <button>Placeholder Button</button>
+      <button>All Orbits</button>
+    </div>
+  );
+  }
+
+export default Buttons;
