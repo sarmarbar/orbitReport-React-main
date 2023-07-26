39762817bc0d975a961b514ab4483af149c0e901
sarmarbar <sarah.smb.barker@gmail.com>
2023-07-26 18:31:05 -0500

    Table adding td and th tags

--- OneDrive/Documents/Projects/LaunchCode/orbitReport-React-main/src/components/Buttons.js
+++ OneDrive/Documents/Projects/LaunchCode/orbitReport-React-main/src/components/Buttons.js
@@ -1,24 +1,28 @@
 import satData from "./satData.js";
-
+//import App from "./App.js";
+//import React from 'react'
+ 
 //import React from 'react'
 //import App from './Tests/App';
 
-const Buttons = () => {
-  
-   
-  
-  
-  
-  
-  
-  
-  
+ const Buttons = (props) => {
+  const {filterByType, setSat, displaySats} = props;
   return (
-   <div>
-      <button>Placeholder Button</button>
+  //<div>{props.filterType} {props.setSat} {props.displayStats}
+<div>
+   {displaySats.map((sat, id) => {
+  return (
+    <button onClick={() => filterByType(sat)} key={id}>
+      {sat} Orbit
+    </button>
+  );
+   })}
+    <button onClick={() => setSat(satData)}></button>
+  
+  
       <button>All Orbits</button>
     </div>
   );
-  }
+  };
 
 export default Buttons;
